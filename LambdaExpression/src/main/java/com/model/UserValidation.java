c1bca36d36baf30ce0d0ee03bd913bf7bba40ed4
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-10-05 18:35:28 +0530

    Add:LastName Validation using lambda

--- LambdaExpression/src/main/java/com/model/UserValidation.java
+++ LambdaExpression/src/main/java/com/model/UserValidation.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
 	public static String FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+	public static String LAST_NAME = "^[A-Z]{1}[a-z]{2,}$";
 	
 	public static Predicate<String> validateUserName = name -> Pattern.matches(FIRST_NAME, name);
 	Scanner sc = new Scanner(System.in);
@@ -18,8 +19,19 @@ public class UserValidation {
         else {
             System.out.println("First Name is Not Vaild");
             System.out.println("First name should starts with Capital letter and has minimum 3 characters");
+        }}
+        public void validateLastName() {
+            System.out.println("Enter Last Name :");
+            String LastName = sc.nextLine();
+            if (validateUserName.test(LastName)) {
+                System.out.println("Last name is Vaild");
+            }
+            else {
+                System.out.println("Last name is Not Vaild");
+                System.out.println("Last name should starts with Capital letter and has minimum 3 characters");
+            }
         }
 		
     }
 
-}
+
--- LambdaExpression/src/main/java/com/model/main.java
+++ LambdaExpression/src/main/java/com/model/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		UserValidation ur=new UserValidation();
 		ur.validateFirstName();
+		ur.validateLastName();
 	}
 
 }
